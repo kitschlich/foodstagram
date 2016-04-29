82f75625e02edd2ca3034268f6f2a5c50432b7d5
Shandra Iannucci <shandrai@gmail.com>
2016-04-29 15:55:34 -0700

    Change getJSON() to $.ajax() to solve CORS issue

--- js/script.js
+++ js/script.js
@@ -8,16 +8,18 @@ $(function(){
 });
 
 function getFbRequest(location){
-  var params = {
-    q: 'restaurant',
-    type: 'place',
-    center: '37.76,-122.427', //location
-    distance: '1000',
-    access_token: '233927430308786|_YCpJw95ivh62Whtfq5rSS7m_Ms'
-  };
-  url = 'https://graph.facebook.com/search';
-
-  $.getJSON(url, params, function(data){
+  $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    url: "https://graph.facebook.com/search",
+    data: {
+      q: 'restaurant',
+      type: 'place',
+      center: '37.76,-122.427', //location
+      distance: '1000',
+      access_token: '233927430308786|_YCpJw95ivh62Whtfq5rSS7m_Ms'
+    }
+  }).done(function(data){
     recordIds(data.data);
   });
 }
@@ -32,41 +34,48 @@ function recordIds(results){
 
 function getInstaIds(fbIds){
   $.each(fbIds, function(index, value){
-    var params = {
-      facebook_places_id: value,
-      access_token: '350763428.1677ed0.93ad0f614ce74560b6538cca9ce32736',
-    };
-    url = 'https://api.instagram.com/v1/locations/search';
-
-    $.getJSON(url, params, function(data){
-      getRecentInsta(data.data[0].id);
+    $.ajax({
+      type: "GET",
+      dataType: "jsonp",
+      url: "https://api.instagram.com/v1/locations/search",
+      data: {
+        facebook_places_id: value,
+        access_token: '350763428.1677ed0.93ad0f614ce74560b6538cca9ce32736'
+      }
+    }).done(function(data){
+      if (parseInt(data.data[0].id) !== 0) {
+        getRecentInsta(data.data[0].id);
+      }
     });
   });
 }
 
 function getRecentInsta(instaId) {
-  var params = {
-    access_token: '350763428.1677ed0.93ad0f614ce74560b6538cca9ce32736'
-  };
-  url = 'https://api.instagram.com/v1/locations/' + instaId + '/media/recent';
+  $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    url: "https://api.instagram.com/v1/locations/" + instaId + "/media/recent",
+    data: {
+      access_token: '350763428.1677ed0.93ad0f614ce74560b6538cca9ce32736'
+    }
+  }).done(function(data){
+    showResults(data);
+  });
+}
 
-  $.getJSON(url, params, function(data){
-    var image, location;
-    //console.log(data);
-    $.each(data.data, function(index, value){
-      //console.log(value.tags);
-      var tags = value.tags;
-      for (var i = 1; i <= tags.length; i++) {
-        if (tags[i-1].indexOf('food') !== -1) {
-          image = value.images.standard_resolution.url;
-          location = value.location.name;
-          console.log(tags[i-1]);
-          $('#search-results').append('<img src="' + image + '"><p>' + location + '</p>');
-          return false;
-        }
+function showResults(recentPosts) {
+  var image, location;
+  $.each(recentPosts.data, function(index, value){
+    var tags = value.tags;
+    for (var i = 1; i <= tags.length; i++) {
+      if (tags[i-1].indexOf('food') !== -1) {
+        image = value.images.standard_resolution.url;
+        location = value.location.name;
+        $('#search-results').append('<img src="' + image + '"><p>' + location + '</p>');
+        return false;
       }
-      });
-    });
+    }
+  });
 }
 
 
